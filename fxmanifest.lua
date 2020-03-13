8937c3dcce602976b3dbbed31c3c6af086af13ef
keiki <sexplosive42@gmail.com>
2020-03-13 09:27:03 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,9 @@
-fx_version 'adamant'
-games { 'gta5' }
+fx_version 'bodacious'
 
-server_script "sv_k9.net.dll"
+game 'gta5'
+
+server_script 'sv_k9.net.dll'
+
 client_scripts {
-    "cl_k9.net.dll",
+    'cl_k9.net.dll',
 } 
